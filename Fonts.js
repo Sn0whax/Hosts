d614af4328e41772130e43ddeecc66391bc81995
Sn0whax <47508022+Sn0whax@users.noreply.github.com>
2023-09-08 23:26:03 -0500

    Create Fonts.js

---
+++ Fonts.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Replace Google Fonts, Microsoft Fonts with Arial
+// @version      1.0
+// @description  Replace Google Fonts, Microsoft Fonts in HTML markup with Arial
+// @author       Sn0whax
+// @icon         https://i.ibb.co/QrGH2vd/Alien-Lines.png
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to replace Google Fonts, Microsoft Fonts in the HTML with Arial
+    function replaceFonts() {
+        var elements = document.querySelectorAll('[href*="fonts.googleapis.com"], [href*="fonts.gstatic.com"], [href*="fs.microsoft.com"]');
+        elements.forEach(function(element) {
+            element.setAttribute('href', '');
+            // Replace font-family declarations with Arial
+            var style = element.closest('style, link[rel="stylesheet"]');
+            if (style) {
+                style.textContent = style.textContent.replace(/['"]?font-family: [^;]*;/g, 'font-family: Arial;');
+            }
+        });
+    }
+
+    // Run the replacement function when the page loads
+    window.addEventListener('load', replaceFonts);
+})();
