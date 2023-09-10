1abd554cd2bc05098e641876479e2d3cb0aec761
Sn0whax <47508022+Sn0whax@users.noreply.github.com>
2023-09-10 11:01:35 -0500

    Update Redirect.js

--- Redirect.js
+++ Redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit and Imgur Redirect
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Redirects Reddit.com to Old.Reddit.com and Imgur.com to alternative domains.
 // @author       Sn0whax
 // @match        https://www.reddit.com/*
@@ -25,7 +25,7 @@
             window.location.href = oldRedditURL;
         } else if ((window.location.hostname === 'imgur.com' || window.location.hostname === 'i.imgur.com') && window.location.hostname !== 'i.redd.it') {
             // Redirect to rimgo.totaldarkness.net or rimgo.us.projectsegfau.lt
-            var newImgurURL = window.location.href.replace(/imgur\.com|i\.imgur\.com/, 'rimgo.totaldarkness.net');
+            var newImgurURL = window.location.href.replace(/imgur\.com|i\.imgur\.com/, 'imgur.artemislena.eu');
             // You can use the alternative domain like this:
             // var newImgurURL = window.location.href.replace(/imgur\.com|i\.imgur\.com/, 'rimgo.us.projectsegfau.lt');
 
